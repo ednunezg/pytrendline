b01bdb6fccea5aead62d9ac912ab53fefdb0cecd
Eduardo Nunez <eng91694@gmail.com>
2021-06-08 21:24:56 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pytrendline",
-    version="1.0.0",
+    version="1.0.1",
     author="Eduardo Nunez",
     author_email="ed@ednunez.me",
     description="Detection for support and resistance trendlines on python",
